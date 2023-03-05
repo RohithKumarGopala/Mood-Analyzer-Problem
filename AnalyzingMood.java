5d517710d93e2e6d5419cf3664e7739b56cbe89f
“rohithgopala44” <rohithgopala44@gmail.com>
2023-03-05 19:34:41 +0530

    Added: Program To Analyze Mood As Per Given Conditions

--- AnalyzingMood.java
+++ AnalyzingMood.java
@@ -1,28 +1,17 @@
 package com.moodAnalyzer;
 
 
-    public class AnalyzingMood extends Exception {
-        public analyzingType type;
+    public class AnalyzingMood{
+            private String message;
+            public AnalyzingMood(String message) {
+                this.message = message;
+            }
 
-        public AnalyzingMood(analyzingType type, String message){
-            super(message);
-            this.type = type;
-        }
+            public String analyseMood() {
+                if(message.contains("Sad"))
+                    return "SAD";
+                else
+                    return "HAPPY";
+            }
 
-        public AnalyzingMood(analyzingType type, Throwable cause) {
-            super(cause);
-            this.type = type;
-        }
-
-        public AnalyzingMood(analyzingType type,String message, Throwable cause){
-            super(message);
-            new AnalyzingMood(type,cause);
-        }
-
-        public enum analyzingType {
-            ENTERED_EMPTY, ENTERED_NULL, NO_SUCH_CLASS, NO_SUCH_METHOD, METHOD_INVOCATION_ISSUE,
-            NO_SUCH_FIELD, ILLEGAL_ACCESS
-        }
     }
-
-
