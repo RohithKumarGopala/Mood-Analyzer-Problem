2a7a12e9cb4205906238bf87fef4f69f1259c5e1
“rohithgopala44” <rohithgopala44@gmail.com>
2023-03-05 19:59:51 +0530

    Added: Program To create A constructor

--- AnalyzingMood.java
+++ AnalyzingMood.java
@@ -1,18 +1,25 @@
 package com.moodAnalyzer;
 
 
-    public class AnalyzingMood {
-        public class AnalyzingMood extends Exception {
-            public exceptionType type;
+public class AnalyzingMood extends Exception {
+    public exceptionType type;
 
-            public AnalyzingMood(String message) {
-                super(message);
-                this.type = type;
-            }
-
-            public enum exceptionType {
-                ENTERED_EMPTY, ENTERED_NULL
-            }
-        }
+    public AnalyzingMood(exceptionType type, String message){
+        super(message);
+        this.type = type;
     }
 
+    public AnalyzingMood(exceptionType type, Throwable cause) {
+        super(cause);
+        this.type = type;
+    }
+
+    public AnalyzingMood(exceptionType type,String message, Throwable cause){
+        super(message);
+        new AnalyzingMood(type,cause);
+    }
+
+    public enum exceptionType {
+        ENTERED_EMPTY, ENTERED_NULL, NO_SUCH_CLASS, NO_SUCH_METHOD, METHOD_INVOCATION_ISSUE
+    }
+}
