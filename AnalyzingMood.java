d2b64d47b0c1712dcdd951c5e302a3c5c4394a06
“rohithgopala44” <rohithgopala44@gmail.com>
2023-03-05 20:05:25 +0530

    Added: Program To Create An Object

--- AnalyzingMood.java
+++ AnalyzingMood.java
@@ -2,24 +2,25 @@ package com.moodAnalyzer;
 
 
 public class AnalyzingMood extends Exception {
-    public exceptionType type;
 
-    public AnalyzingMood(exceptionType type, String message){
-        super(message);
-        this.type = type;
-    }
+        public exceptionType type;
 
-    public AnalyzingMood(exceptionType type, Throwable cause) {
-        super(cause);
-        this.type = type;
-    }
+        public AnalyzingMood(String message) {
+            super(message);
+            this.type = type;
+        }
 
-    public AnalyzingMood(exceptionType type,String message, Throwable cause){
-        super(message);
-        new AnalyzingMood(type,cause);
-    }
+        public AnalyzingMood(exceptionType type, Throwable cause) {
+            super(cause);
+            this.type = type;
+        }
 
-    public enum exceptionType {
-        ENTERED_EMPTY, ENTERED_NULL, NO_SUCH_CLASS, NO_SUCH_METHOD, METHOD_INVOCATION_ISSUE
+        public AnalyzingMood(exceptionType type, String message, Throwable cause) {
+            super(message, cause);
+            this.type = type;
+        }
+
+        public enum exceptionType {
+            ENTERED_EMPTY, ENTERED_NULL, NO_SUCH_CLASS, NO_SUCH_METHOD, METHOD_INVOCATION_ISSUE
+        }
     }
-}
